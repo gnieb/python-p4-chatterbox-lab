1f6f458a6580dffad25273ef43e9d82ebf4c3e20
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-13 12:03:16 -0400

    methods and routes complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [m.to_dict() for m in Message.query.order_by('created_at').all()]
+        return make_response(messages, 200)
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'PATCH':
+        message = Message.query.filter_by(id=id).first()
+        if not message:
+             return make_response({"error":"message not found!"}, 404)
+        
+        data = request.get_json()
+        for key in data.keys():
+            setattr(message, key, data[key])
+        db.session.add(message)
+        db.session.commit()
+        return make_response(message.to_dict(), 200)
+
+    elif request.method =='DELETE':
+        message = Message.query.filter_by(id=id).first()
+        if not message:
+            return make_response({"error":"message not found!"}, 404)
+        db.session.delete(message)
+        db.session.commit()
+        return make_response({}, 202)
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
